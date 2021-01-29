7b59ac084eaebacdd453af2dcafb553b7d6468ba
ernestchng <ernestchngyongcheng97@gmail.com>
2021-01-29 23:59:42 +0800

    Added fomc_speeches

---
+++ scripts/data/fomc_speeches.py
@@ -0,0 +1,72 @@
+import pandas as pd
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+BASE_URL = 'https://www.federalreserve.gov'
+
+print("Launching webdriver...")
+driver = webdriver.Chrome('chromedriver')
+
+print("Collecting data...")
+
+all_data = []
+counter = 0
+
+# Archive 2005 below
+START_YEAR = 2006
+END_YEAR = 2021
+
+for year in range(START_YEAR, END_YEAR + 1):
+    if year >= 2011:
+        speech_url = f'{BASE_URL}/newsevents/speech/{year}-speeches.htm'
+    else:
+        speech_url = f'{BASE_URL}/newsevents/speech/{year}speech.htm'
+
+    driver.get(speech_url)
+
+    print(f"\nCollecting from Year {year}...")
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    posts = soup.find('div', class_='row eventlist').find_all('div', class_='row')
+
+    for post in posts:
+        try:
+            counter += 1
+            print(f"Collecting post #{counter}...")
+            date = pd.to_datetime(post.find('time').text)
+
+            title = post.find('a').text
+            link = post.find('a').get('href')
+            speaker = post.find('p', class_='news__speaker').text
+            location = post.find_all('p')[-2].text
+
+            # retrieving text from link
+            post_url = BASE_URL + link
+            driver.get(post_url)
+            html = driver.page_source
+            inner_soup = BeautifulSoup(html, 'html.parser')
+
+            paragraphs = inner_soup.find('div', class_='col-xs-12 col-sm-8 col-md-8').find_all('p')
+            text = ''
+            for paragraph in paragraphs:
+                if not paragraph.find('a'):
+                    text += paragraph.text
+
+            all_data.append({
+                'date': date,
+                'title': title,
+                'link': link,
+                'speaker': speaker,
+                'location': location,
+                'text': text
+            })
+        except Exception as e:
+            print(e)
+            continue
+
+print("\nCollection completed!")
+driver.close()
+
+df = pd.DataFrame(all_data).drop_duplicates()
+df.to_csv('data/meta/fomc_speeches.csv', index=False)
