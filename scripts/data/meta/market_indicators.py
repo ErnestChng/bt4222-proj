0b9c28eb37e4539d7209b2609aa786bbc5e930d7
Jolene <jolene0443@gmail.com>
2021-02-05 13:38:10 +0800

    Changed to Fred Api

--- scripts/data/meta/market_indicators.py
+++ scripts/data/meta/market_indicators.py
@@ -1,12 +1,12 @@
-import quandl
 import os
 import pandas as pd
+from fredapi import Fred
 
 dir = '/Users/jolene/Downloads/1.BT4222/Group Project/bt4222-proj'
-quandl.ApiConfig.api_key = '7zBDzsuMEcutufUJLKwc'
-from_date = '2006-01-01'
-indicators = ('DFEDTARL',  # FED rate lower bound
-              'DFEDTARU',  # FED rate upper bound
+fred = Fred(api_key='2fb48248f0ce3781c82ffca58fecfb36')
+start_date = '1/1/2006'
+indicators = ('DFEDTARL',  # Federal Funds Target Range - lower bound
+              'DFEDTARU',  # Federal Funds Target Range - upper bound
               'GDPC1',     # Real GDP
               'CPIAUCSL',  # Consumer Price Index (CPI)
               'UNRATE',    # Unemployment Rate
@@ -18,16 +18,16 @@ indicators = ('DFEDTARL',  # FED rate lower bound
 full_data = pd.DataFrame()
 
 for indicator in indicators:
-    data = quandl.get(f"FRED/{indicator}", start_date=from_date)
+    df = pd.DataFrame(fred.get_series(indicator, observation_start=start_date))
 
     if full_data.empty:
-        full_data = data
+        full_data = df
     else:
-        full_data = pd.concat([full_data, data], axis=1)
+        full_data = pd.concat([full_data, df], axis=1)
 
 
 full_data.columns = list(indicators)
-full_data.to_csv(os.path.join(dir, './data/meta/market_data.csv'))
+full_data.to_csv(os.path.join(dir, './data/meta/api_market_data.csv'))
 
 full_data = full_data.ffill()
-full_data.to_csv(os.path.join(dir, './data/meta/market_data_filled.csv'))
+full_data.to_csv(os.path.join(dir, './data/meta/api_market_data_filled.csv'))
