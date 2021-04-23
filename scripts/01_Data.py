9aad8c5944e92180e5e3f74d4e7b34310d23b96b
ernestchng <ernestchngyongcheng97@gmail.com>
2021-04-24 02:33:10 +0800

    refactor: Commented out press_conference

--- scripts/01_Data.py
+++ scripts/01_Data.py
@@ -7,7 +7,7 @@ import pandas as pd
 from data.macro.macro_data import retrieve_macro_data
 from data.textual.fomc_calendar import retrieve_fomc_calendar
 from data.textual.fomc_minutes import retrieve_fomc_minutes
-from data.textual.fomc_press_conf import retrieve_fomc_press_conf
+# from data.textual.fomc_press_conf import retrieve_fomc_press_conf
 from data.textual.fomc_speeches import retrieve_fomc_speeches
 from data.textual.fomc_statements import retrieve_fomc_statements
 from data.textual.fomc_testimony import retrieve_fomc_testimony
@@ -30,7 +30,7 @@ if __name__ == "__main__":
 
         #### Scraping FOMC data ####
         minutes = retrieve_fomc_minutes()
-        press_conf = retrieve_fomc_press_conf()  # requires pdf files
+        # press_conf = retrieve_fomc_press_conf()  # requires pdf files
         speeches = retrieve_fomc_speeches()
         statements = retrieve_fomc_statements()
         testimony = retrieve_fomc_testimony()
@@ -39,13 +39,13 @@ if __name__ == "__main__":
         macro = retrieve_macro_data()
 
         #### Writing data ####
-        minutes.to_csv('data/textual/fomc_minutes.txt', sep=',', index=False)
-        press_conf.to_csv('data/textual/fomc_press_conf.txt', sep=',', index=False)
-        speeches.to_csv('data/textual/fomc_speeches.txt', sep=',', index=False)
-        statements.to_csv('data/textual/fomc_statements.txt', sep=',', index=False)
-        testimony.to_csv('data/textual/fomc_testimony.txt', sep=',', index=False)
-        calendar.to_csv('data/textual/fomc_calendar.txt', sep=',', index=False)
-        macro.to_csv('data/macro/macro.csv')
+        # minutes.to_csv('data/textual/fomc_minutes.txt', sep=',', index=False)
+        # press_conf.to_csv('data/textual/fomc_press_conf.txt', sep=',', index=False)
+        # speeches.to_csv('data/textual/fomc_speeches.txt', sep=',', index=False)
+        # statements.to_csv('data/textual/fomc_statements.txt', sep=',', index=False)
+        # testimony.to_csv('data/textual/fomc_testimony.txt', sep=',', index=False)
+        # calendar.to_csv('data/textual/fomc_calendar.txt', sep=',', index=False)
+        # macro.to_csv('data/macro/macro.csv')
 
         print("==========Done==========\n")
         print(f"Time taken for data collection: {time.time() - start_time}")
